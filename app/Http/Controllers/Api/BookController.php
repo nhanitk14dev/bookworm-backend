49d0cc7a5d933765feba2971d7ea64eae992aa6e
Nhan David <nhanitk14dev@gmail.com>
2021-07-01 14:33:39 +0700

    Book customer reviews update calculate total avg rating star

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -158,7 +158,15 @@ class BookController extends Controller
 
     public function getReviews($book_id, Request $request)
     {
-        $reviews = Review::where('book_id', $book_id);
+        $reviews   = Review::where('book_id', $book_id);
+        $meta_data = [
+            'avg_rating_star' => number_format($reviews->get()->avg('rating_star'), 1),
+            'five_star'       => $reviews->get()->where('rating_star', 5)->count(),
+            'four_star'       => $reviews->get()->where('rating_star', 4)->count(),
+            'three_star'      => $reviews->get()->where('rating_star', 3)->count(),
+            'two_star'        => $reviews->get()->where('rating_star', 2)->count(),
+            'one_star'        => $reviews->get()->where('rating_star', 1)->count(),
+        ];
 
         if ($request->query('sortByKey') == 'oldest') {
             $reviews->orderBy('review_date', 'asc');
@@ -170,8 +178,9 @@ class BookController extends Controller
         $result   = empty($per_page) ? $reviews->paginate(10) : $reviews->paginate((int) $per_page);
 
         return response([
-            'reviews' => $result,
-            'code'    => RESPONSE_CODES['request_success'],
+            'reviews'   => $result,
+            'meta_data' => $meta_data,
+            'code'      => RESPONSE_CODES['request_success'],
         ], 200);
     }
 }
