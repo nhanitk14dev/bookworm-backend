014e35d78ac436fa53ff2dfc98bd3ec69c5d5ba0
Nhan David <nhanitk14dev@gmail.com>
2021-07-06 20:06:14 +0700

    Update route api and create new Review controller

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -5,11 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\BookResource;
 use App\Models\Book;
-use App\Models\Review;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Validator;
 
 class BookController extends Controller
 {
@@ -160,70 +158,5 @@ class BookController extends Controller
             'totals'        => count($data),
             'code'          => RESPONSE_CODES['request_success'],
         ], 200);
-    }
-
-    public function getReviews($book_id, Request $request)
-    {
-        $reviews   = Review::where('book_id', $book_id);
-        $meta_data = [
-            'avg_rating_star' => number_format($reviews->get()->avg('rating_star'), 1),
-            'five_star'       => $reviews->get()->where('rating_star', 5)->count(),
-            'four_star'       => $reviews->get()->where('rating_star', 4)->count(),
-            'three_star'      => $reviews->get()->where('rating_star', 3)->count(),
-            'two_star'        => $reviews->get()->where('rating_star', 2)->count(),
-            'one_star'        => $reviews->get()->where('rating_star', 1)->count(),
-        ];
-
-        if ($request->query('sortByKey') == 'oldest') {
-            $reviews->orderBy('review_date', 'asc');
-        } else {
-            $reviews->orderByDesc('review_date');
-        }
-
-        $per_page = $request->query('perPage');
-        $result   = empty($per_page) ? $reviews->paginate(10) : $reviews->paginate((int) $per_page);
-
-        return response([
-            'reviews'   => $result,
-            'meta_data' => $meta_data,
-            'code'      => RESPONSE_CODES['request_success'],
-        ], 200);
-    }
-
-    public function storeReview($book_id, Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'review_title' => 'required|max:120',
-        ]);
-
-        if ($validator->fails()) {
-            return response([
-                'message' => $validator->errors()->first(),
-                'code'    => RESPONSE_CODES['item_not_failed'],
-            ], 200);
-        }
-
-        $result = [];
-        $book   = Book::find($book_id);
-        if ($book) {
-            DB::transaction(function () use ($book_id, $request) {
-                $review                = new Review();
-                $review->book_id       = $book_id;
-                $review->review_title  = $request->input('review_title');
-                $review->review_detail = $request->input('review_detail');
-                $review->rating_star   = $request->input('rating_star');
-                $review->review_date   = date('Y-m-d H:i:s');
-                $review->save();
-            });
-
-            $result = array('code' => RESPONSE_CODES['request_success']);
-        } else {
-            $result = [
-                'message' => 'The current book is not found',
-                'code'    => RESPONSE_CODES['item_not_found'],
-            ];
-        }
-
-        return response($result, 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\Api\AuthorController;
 use App\Http\Controllers\Api\BookController;
-use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\ReviewController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,19 +18,28 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
  */
-use Illuminate\Support\Facades\Route;
 
-// Route definition...
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/book/{slug}', [BookController::class, 'show']);
-Route::get('/discount-books', [BookController::class, 'getDiscountBooks']);
-Route::get('/recommended-books', [BookController::class, 'getRecommendedBooks']);
-Route::get('/popular-books', [BookController::class, 'getPopularBooks']);
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/authors', [AuthorController::class, 'index']);
-Route::get('/reviews/{book_id}', [BookController::class, 'getReviews']);
-Route::post('/create-reviews/{book_id}', [BookController::class, 'storeReview']);
-Route::post('/create-order', [OrderController::class, 'store']);
+Route::prefix('v1')->group(function () {
+    Route::get('/books/discounts', [BookController::class, 'getDiscountBooks']);
+    Route::get('/books/popularity', [BookController::class, 'getPopularBooks']);
+    Route::get('/books/recommendation', [BookController::class, 'getRecommendedBooks']);
+
+    Route::resource('books', BookController::class)->only([
+        'index', 'show',
+    ]);
+    Route::resource('categories', CategoryController::class)->only([
+        'index',
+    ]);
+    Route::resource('orders/{book_id}', OrderController::class)->only([
+        'store',
+    ]);
+    Route::resource('reviews/{book_id}', ReviewController::class)->only([
+        'index', 'store',
+    ]);
+    Route::resource('authors', AuthorController::class)->only([
+        'index',
+    ]);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
